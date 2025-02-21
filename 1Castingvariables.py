9dfd0d05851d22e01968a31b4e2868bd56a3ce0b
Marcus <marcus.o'hanlon@ons.gov.uk>
2025-02-21 16:25:49 +0000

    yo

---
+++ 1Castingvariables.py
@@ -0,0 +1,9 @@
+length = int(input ("what is da length: "))
+width = int(input ("wat is da width: "))
+
+area = (length * width)
+print (f"the area is:  {area}")
+
+perimiter = (length + length + width + width)
+print (f"the perimiter is {perimiter}")
+23
