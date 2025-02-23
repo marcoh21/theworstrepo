c241f93acdac9893ed7d84281fddea2e8a3e5427
marcoh21 <marcus.ohanlm68@gmail.com>
2025-02-23 23:41:45 +0000

    yo

---
+++ bcstuff/Typecasting.py
@@ -0,0 +1,22 @@
+name = "bruh"
+age = "21"
+gpa = 4.0
+student = False
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
+
+
+age = float(age)
+print(age)
+
+gpa = int(gpa)
+print(gpa)
+
+student = str(student)
+print(student)
+
+name = bool(name)
+print(name)
---
+++ bcstuff/conditionalexpression.py
@@ -0,0 +1,3 @@
+num = 6
+
+print ("even" if num % 2 == 0 else "odd") # this is a conditional expression
